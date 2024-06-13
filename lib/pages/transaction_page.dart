b5be4f816e4999c52c7f89552fc08c0c76b560d0
fitrianinurulazizah <fitrianiitsme203@gmail.com>
2024-06-13 15:35:35 +0700

    update

--- lib/pages/transaction_page.dart
+++ lib/pages/transaction_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class TransactionPage extends StatefulWidget {
   const TransactionPage({super.key});
@@ -8,16 +10,139 @@ class TransactionPage extends StatefulWidget {
 }
 
 class _TransactionPageState extends State<TransactionPage> {
+  bool isExpense = true;
+  List<String> list = ['Makan','Transport','Nonton Film','Jalan-Jalan','Pendidikan'];
+  late String dropDownValue = list.first;
+  TextEditingController dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: 
-      Text("Add Transaction")),
+      appBar: AppBar(
+        title: Text(
+          "Add Transaction",
+          style: GoogleFonts.montserrat(fontSize: 20),
+        ),
+        backgroundColor: Color.fromARGB(255, 241, 155, 210),
+      ),
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Column(
-            children: [],
-            )),),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // start di sebelah kiri
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      isExpense ? 'Expense' : 'Income',
+                      style: GoogleFonts.montserrat(fontSize: 14),
+                    ),
+                    Switch(
+                      value: isExpense,
+                      onChanged: (bool value) {
+                        setState(() {
+                          isExpense = value;
+                        });
+                      },
+                      inactiveTrackColor: Color.fromARGB(255, 188, 145, 160),
+                      inactiveThumbColor: Colors.pink,
+                      activeColor: Colors.pink,
+                    ),
+                  ],
+                ),
+
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      labelText: "Jumlah"
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                        'Kategori',
+                        style: GoogleFonts.montserrat(fontSize: 16),
+                      ),
+                ),
+
+                //mengambil kategori
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: DropdownButton<String>(
+                    value: dropDownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    items: list.map<DropdownMenuItem<String>>((String value){
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String ? value) {}
+                  ),
+
+                  
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    readOnly: true,
+                    controller: dateController,
+                    decoration: InputDecoration(
+                      labelText: "Tanggal"),
+                      onTap: () async{
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context, 
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2010),
+                          lastDate: DateTime(2099));
+                  
+                          if (pickedDate != null){
+                            String formattedDate = 
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                  
+                            dateController.text = formattedDate;
+                          }
+                      },
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 241, 155, 210), // background color
+                    ), 
+                    child: Text
+                    ("Simpan",
+                    style: TextStyle(
+                        color: Colors.white, // text color
+                      ),
+                      ),),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
